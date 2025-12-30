7541ea606f2a3e8b5a4d6797c9909ce17947d437
william <espiw@farmingdale.edu>
2025-12-30 14:42:33 -0500

    calculator functionality complete. appendToDisplay function adds inputted buttons to the display, clearDisplay function clears any input currently in the display and calculate function uses try/catch block to evaluate any input, calculate it and catches any potentially input errors

--- calculator.js
+++ calculator.js
@@ -1,1 +1,26 @@
 // ---------- CALCULATOR FUNCTIONALITY ---------- //
+
+const display = document.getElementById("display");
+
+// this function adds the buttons pressed to the display
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+// this function clears the display
+function clearDisplay() {
+    display.value = "";
+}
+
+// this function handles the actual calculating of the calculator
+function calculate() {
+    // use try/catch in case of any potential error inputs
+    // example 3.14 + (no value) catches the error and displays error msg
+    try {
+        // eval() grabs any input and evaluates it (basically calculates)
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
